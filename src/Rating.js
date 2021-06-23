116f80551b1f54c26d898089a1111d17462a76f4
KotiMango <rons20000@gmail.com>
2021-06-23 23:38:17 +0300

    initialized a reset button

--- src/Rating.js
+++ src/Rating.js
@@ -23,10 +23,26 @@ export default function Rating({ value }) {
           );
         })}
       </div>
+      <ResetBtn onClick={() => setVal(0)}>RESET</ResetBtn>
     </Stars>
   );
 }
 
 const Stars = styled.div`
   width: 1000px;
+`;
+const ResetBtn = styled.button`
+  width: 8%;
+  font-weight: bold;
+  background: transparent;
+  border: 2px solid #bae0ff;
+  padding: 1rem 1rem;
+  color: #0a3a40;
+  text-align: center;
+  margin-top: 5px;
+  cursor: pointer;
+  &:hover {
+    background-color: #bae0ff;
+    color: white;
+  }
 `;
